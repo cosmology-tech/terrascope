e12b2e58e28166f9436dcad7fb84cb479b56d86a
Dan Lynch <pyramation@gmail.com>
2023-07-30 14:36:23 +0200

    upgrade

--- types/codegen/ibc/applications/fee/v1/query.lcd.d.ts
+++ types/codegen/ibc/applications/fee/v1/query.lcd.d.ts
@@ -1,4 +1,4 @@
-import { LCDClient } from "@osmonauts/lcd";
+import { LCDClient } from "@cosmology/lcd";
 import { QueryIncentivizedPacketsRequest, QueryIncentivizedPacketsResponseSDKType, QueryIncentivizedPacketRequest, QueryIncentivizedPacketResponseSDKType, QueryIncentivizedPacketsForChannelRequest, QueryIncentivizedPacketsForChannelResponseSDKType, QueryTotalRecvFeesRequest, QueryTotalRecvFeesResponseSDKType, QueryTotalAckFeesRequest, QueryTotalAckFeesResponseSDKType, QueryTotalTimeoutFeesRequest, QueryTotalTimeoutFeesResponseSDKType, QueryCounterpartyAddressRequest, QueryCounterpartyAddressResponseSDKType, QueryFeeEnabledChannelsRequest, QueryFeeEnabledChannelsResponseSDKType, QueryFeeEnabledChannelRequest, QueryFeeEnabledChannelResponseSDKType } from "./query";
 export declare class LCDQueryClient {
     req: LCDClient;
